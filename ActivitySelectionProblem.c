34d6901ca0c573b35c0551bcd0741a2b237e06dc
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-01-09 10:53:37 +0200

    Use built-in sorting function.
    It is much more optimized than a manual implementation, and significantly lowers the maintenance burden of the project's code.

--- ActivitySelectionProblem.c
+++ ActivitySelectionProblem.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void findMaxActivities(int n,int arr1[n],int depr[n]);
-void quickSort(int a[],int l,int h);
-int partition(int a[],int l,int h);
 
 int main(){
     printf("Give number of activities: ");
@@ -20,64 +19,35 @@ int main(){
     return 0;
 }
 
+int compareIntegers(void *x1,void *x2){
+    return *(int *) x1 - *(int *) x2;
+}
+
 void findMaxActivities(int n,int arrl[n],int depr[n]){
-    quickSort(arrl,0,n-1);                          //sort the tables(you can use your own sort method)
-    quickSort(depr,0,n-1);
+    qsort((void *) arrl, n, sizeof(int), compareIntegers); //sort the tables
+    qsort((void *) depr, n, sizeof(int), compareIntegers);
     int depr_time[n];
     int activitiesIn = 1,maxActivities = 1;
     depr_time[0] = depr[0];
     int i = 1, j = 0,k = 0;
 
-    while (i < n && j < n){                        
+    while (i < n && j < n){
         if (arrl[i] < depr[j]){
-            activitiesIn++;                           
+            activitiesIn++;
             if (activitiesIn >= maxActivities){
                 if(activitiesIn == maxActivities)k++;   //k is to keep the number of the same activities in 2 or more time periods
-                else k = 0;                        
+                else k = 0;
                 maxActivities = activitiesIn;
                 depr_time[k] = depr[j];           //departure time of max activity
             }
             i++;
         }
         else{
-            activitiesIn--;          
-            j++;                     
+            activitiesIn--;
+            j++;
         }
     }
     for(i=0; i<=k; i++){
         printf("Maximum Number of Activities : %d at time period %d-%d\n",maxActivities,depr_time[i]-1,depr_time[i]);
     }
-}
-
-void quickSort(int a[],int l,int h){
-    int j;
-    if(l<h){
-        j=partition(a,l,h);
-        quickSort(a,l,j-1);
-        quickSort(a,j+1,h);
-    }
-}
-
-int partition(int a[],int l,int h){
-    int v,i,j,temp;
-    v=a[l];
-    i=l;
-    j=h+1;
-
-    do{
-        do{
-            i++;
-        }while(a[i]<v&&i<=h);
-        do{
-            j--;
-        }while(v<a[j]);
-        if(i<j){
-            temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
-        }
-    }while(i<j);
-    a[l]=a[j];
-    a[j]=v;
-    return j;
 }
